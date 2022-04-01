601a31e359f26aeb643b827526353874373a8abd
pravashupreti <pravashupreti@gmail.com>
2022-04-01 14:44:31 -0500

    Add day5 assignment

---
+++ js/day5.js
@@ -0,0 +1,22 @@
+function sum(list) {
+    return list.filter(x => x > 20).reduce((acc, item) => acc + item)
+}
+
+let numberList = [10, 20, 30, 40, 50]
+console.log(sum(numberList))
+
+function getNewArray(list) {
+    return list.filter(x => {
+            return x.length >= 5
+        })
+        .filter(x => {
+            return x.indexOf('a') > 0
+        })
+}
+
+let stringList = [
+    "Pravash",
+    "Raj",
+    "Upreti"
+]
+console.log(getNewArray(stringList))
