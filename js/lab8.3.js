415a604db8aa90904891741f44913aad57b5f72b
pravashupreti <pravashupreti@gmail.com>
2022-04-10 07:07:11 -0500

    Add Q8.3

---
+++ js/lab8.3.js
@@ -0,0 +1,19 @@
+// Q1.
+"use strict";
+
+Array.prototype.sort = function() {
+    let sortedArr = this
+    for (let i = 0; i < sortedArr.length; i++) {
+        for (let j = 0; j < sortedArr.length - i - 1; j++) {
+
+            if (sortedArr[j] > sortedArr[j + 1]) {
+                let tmp = sortedArr[j + 1]
+                sortedArr[j + 1] = sortedArr[j]
+                sortedArr[j] = tmp
+            }
+        }
+    }
+    return sortedArr
+}
+
+console.log([5, 6, 8, 9, 7, 4, 1].sort())
